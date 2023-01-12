db07b5db7023b794f0b9f1b438c3acacafb4287f
coxpert <alektyler14@hotmail.com>
2023-01-12 13:49:58 -0500

    Simple tasks CRUD using Laravel Sanctum.

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Resources\TasksRecourse;
+use App\Models\Task;
+use App\Traits\HttpResponses;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TasksController extends Controller
+{
+    use HttpResponses;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return TasksRecourse::collection(
+            Task::where('user_id', Auth::user()->id)->get()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTaskRequest $request)
+    {
+        $request->validated($request->all());
+
+        $task = Task::create([
+           'user_id' => Auth::user()->id,
+           'name' => $request->name,
+           'description' => $request->description,
+           'priority' => $request->priority
+        ]);
+
+        return new TasksRecourse($task);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        if(Auth::user()->id !==  $task->user_id) {
+            return $this->error('', 'You are not authorized to make this request', 403);
+        }
+        return new TasksRecourse($task);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Task $task)
+    {
+        if(Auth::user()->id !==  $task->user_id) {
+            return $this->error('', 'You are not authorized to make this request', 403);
+        }
+
+        $task->update($request->all());
+
+        return new TasksRecourse($task);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Task $task)
+    {
+        if(Auth::user()->id !==  $task->user_id) {
+            return $this->error('', 'You are not authorized to make this request', 403);
+        }
+
+        $task->delete();
+
+        return response(null, 204);
+    }
+
+
+}
